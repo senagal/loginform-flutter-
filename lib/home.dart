6d9791ad8d33199c4758d8d05ba0b9d6dac4df13
senagal <senaabdisa4kilo@gmail.com>
2025-04-25 08:12:21 +0300

    Changed the style(color etc) and added validation for empty response.

--- lib/home.dart
+++ lib/home.dart
@@ -9,61 +9,102 @@ class LoginForm extends StatefulWidget {
 
 class _LoginFormState extends State<LoginForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _email;
+  String? _password;
 
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  final String _correctEmail = 'sena@elective.com';
+  final String _correctEmail =
+      'sena@elective.com'; //here is the correct emal i used
   final String _correctPassword = '123456';
 
-  void _validateLogin() {
-    final email = _emailController.text;
-    final password = _passwordController.text;
+  String? _validateEmail(String? value) {
+    return (value == null || value.isEmpty) ? 'Email is required' : null;
+  }
 
-    if (email == _correctEmail && password == _correctPassword) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('Login successful!')));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Invalid email or password')),
-      );
+  String? _validatePassword(String? value) {
+    return (value == null || value.isEmpty) ? 'Password is required' : null;
+  }
+
+  void _validateLogin() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      if (_email == _correctEmail && _password == _correctPassword) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Login successful!')));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Invalid email or password')),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(
+        title: const Text(
+          'Login Form',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(color: Colors.limeAccent),
+        backgroundColor: const Color.fromARGB(255, 1, 44, 4),
+      ),
       body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Padding(
+        child: Center(
+          child: Container(
+            height: 400,
             padding: const EdgeInsets.all(32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'user@elective.com',
+            margin: const EdgeInsets.symmetric(horizontal: 300.0),
+            decoration: BoxDecoration(
+              color: Colors.lime,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'user@elective.com',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: _validateEmail,
+                    onSaved: (value) => _email = value,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter password',
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter password',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: _validatePassword,
+                    onSaved: (value) => _password = value,
                   ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 30.0),
-                ElevatedButton(
-                  onPressed: _validateLogin,
-                  child: const Text('Login'),
-                ),
-              ],
+                  const SizedBox(height: 30.0),
+                  ElevatedButton(
+                    onPressed: _validateLogin,
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 1, 44, 4),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Form Validation Demo',
+      title: 'Login Form Demo',
       home: const LoginForm(),
     );
   }
